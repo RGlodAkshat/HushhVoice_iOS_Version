b7dcec896fa1ea229ff21a5eeb98bae9700e2b8e
RGlodAkshat <ak8akshat@gmail.com>
2025-10-16 10:18:19 +0530

    Initial Commit

---
+++ HushhVoice/HushhVoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  HushhVoiceApp.swift
+//  HushhVoice
+//
+//  Created by Akshat Kumar on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct HushhVoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
